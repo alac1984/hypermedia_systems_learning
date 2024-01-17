69a877cec3f8eb20d8b37d187084d5b05992f17e
André Carvalho <alac1984@gmail.com>
2024-01-16 23:49:00 -0300

    Tests on repo_retrieve_contacts done

--- repository/contact.py
+++ repository/contact.py
@@ -5,8 +5,10 @@ from models.contact import Contact
 
 
 async def repo_retrieve_contacts(session: AsyncSession, q: str = "") -> list[Contact]:
-    query = text(f"select * from contact where to_tsvector('english', first_name) @@ plainto_tsquery('english', :query)")
-    results = await session.execute(query, {'query': q})
+    query = text(
+        "select * from contact where to_tsvector('english', first_name)"
+        " @@ plainto_tsquery('english', :query)"
+    )
+    results = await session.execute(query, {"query": q})
 
-    return [Contact(**result) for result in results]
-
+    return [Contact(**result) for result in results.mappings()]
--- tests/test_repository/test_contact.py
+++ tests/test_repository/test_contact.py
@@ -5,6 +5,7 @@ from repository.contact import repo_retrieve_contacts
 
 @pytest.mark.asyncio
 async def test_repo_retrieve_contacts(session):
-    contacts = await repo_retrieve_contacts(session)
+    contacts = await repo_retrieve_contacts(session, "Linda")
 
     assert contacts is not None
+    assert len(contacts) == 2
